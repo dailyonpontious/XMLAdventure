12113c3fe78a535bed02439f2ddd8e0785c9a15f
BusterBear1 <rwettstein@student.neumont.edu>
2024-10-16 19:38:07 -0600

    stuff

--- src/main/java/csc180/Pontious/Dailyon/Main.java
+++ src/main/java/csc180/Pontious/Dailyon/Main.java
@@ -1,7 +1,28 @@
 package csc180.Pontious.Dailyon;
 
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
     public static void main(String[] args) {
+        try{
+            String url = "jdbc:mysql://localhost:3306/xmladventure"; // database link
+            String user = "root";
+            String pass = "Test";
+            Connection connection = DriverManager.getConnection(url,user,pass);
 
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser parser = factory.newSAXParser();
+
+            MyHandler handler = new MyHandler(connection);
+
+            parser.parse("C:\\Users\\RWettstein\\Desktop\\PRO 335\\XMLAdventure\\customers\\customers.xml", handler);
+
+            connection.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
